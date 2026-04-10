35c73fac990a7faaaa46ebbb44a55dc98095eb75
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-09 22:04:24 -0300

    feat - aula 04/09

--- src/types/especialidade.ts
+++ src/types/especialidade.ts
@@ -1,5 +1,5 @@
-export type Especialidade = { 
-    id: number;
-    nome: string;
-    descricao?: string; 
-}; 
+export type Especialidade = {
+  id: number;
+  nome: string;
+  descricao?: string;
+};
--- src/types/statusConsulta.ts
+++ src/types/statusConsulta.ts
@@ -1,4 +1,4 @@
-export type StatusConsulta = 
+export type StatusConsulta =
   | "agendada"
   | "confirmada"
   | "cancelada"
