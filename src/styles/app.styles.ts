6aa273e47b294174f12e7c35f330ac98a87eb1ea
mneugebauer-integration <mneugebauer@integrationconsulting.com.br>
2026-04-13 08:55:17 -0300

    fix: alterando fundo de tela

--- src/styles/app.styles.ts
+++ src/styles/app.styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#79059C",
+    backgroundColor: "#ADD8E6",
   },
   scrollContent: {
     padding: 20,
